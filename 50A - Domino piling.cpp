9fa769de7ff2566363c19029e7fc3791cbb5ad2f
nileshdas <39062229+nileshdas@users.noreply.github.com>
2019-05-30 12:06:59 +0530

    Add files via upload

---
+++ 50A - Domino piling.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include <bits/stdc++.h> 
+
+
+using namespace std;
+
+int main() {
+	// your code goes here
+    int n,m;
+    cin>>m>>n;
+    int x = m*n/2;
+    cout<<x<<endl;
+      
+    return 0;
+    
+}
